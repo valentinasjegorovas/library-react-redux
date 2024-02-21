f0167d2fec7bdd17776b2f13fe26eb0b02f20a18
valentinasjegorovas <150137237+valentinasjegorovas@users.noreply.github.com>
2024-02-21 20:46:58 +0200

    Add files via upload
    library-react-redux

---
+++ library-react-redux/frontend/src/redux/slices/booksSlice.js
@@ -0,0 +1,74 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import createBookWithID from '../../utils/createBookWithID';
+import axios from 'axios';
+import { setError } from './errorSlice';
+
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+};
+
+export const fetchBook = createAsyncThunk(
+  'books/fetchBook',
+  async (url, thunkAPI) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error) {
+      thunkAPI.dispatch(setError(error.message));
+      throw error;
+    }
+  }
+);
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    deleteBook: (state, action) => {
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      };
+    },
+    toggleFavorite: (state, action) => {
+      state.books.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
+        }
+      });
+    },
+  },
+
+  extraReducers: {
+    [fetchBook.pending]: (state) => {
+      state.isLoadingViaAPI = true;
+    },
+    [fetchBook.fulfilled]: (state, action) => {
+      state.isLoadingViaAPI = false;
+      if (action.payload.title && action.payload.author) {
+        state.books.push(createBookWithID(action.payload, 'API'));
+      }
+    },
+    [fetchBook.rejected]: (state) => {
+      state.isLoadingViaAPI = false;
+    },
+  },
+
+  /*   extraReducers: (builder) => {
+    builder.addCase(fetchBook.fulfilled, (state, action) => {
+      if (action.payload.title && action.payload.author) {
+        state.books.push(createBookWithID(action.payload, 'API'));
+      }
+    });
+  }, */
+});
+
+export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
+export const selectBooks = (state) => state.books.books;
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI;
+
+export default booksSlice.reducer;
